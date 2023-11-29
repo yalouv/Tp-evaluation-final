4c45cb0b378eba7282f3e6f3fc83ae4eb2699d3b
Yanis Louvel <yanis.louvel@gmail.com>
2023-11-29 16:48:41 +0100

    nouvelle mise a jour

--- server-info.py
+++ server-info.py
@@ -5,4 +5,6 @@ memory = psutil.virtual_memory()
 
 # Print the memory usage details
 print("Total Memory: {:.2f} GB".format(memory.total / (1024 ** 3)))
-print("Available Memory: {:.2f} GB".format(memory.available / (1024 ** 
+print("Available Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
+print("Used Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
+print("Memory Percent: {:.2f}%".format(memory.percent))
